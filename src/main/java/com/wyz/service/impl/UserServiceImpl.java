9c7b5ae184ba4cf899de7e6bb6686305fa458669
wyz <wyz@ergoz.io>
2022-05-27 00:26:51 +0800

    commit 1

--- src/main/java/com/wyz/service/impl/UserServiceImpl.java
+++ src/main/java/com/wyz/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +18,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public void doSomething(User user){
+        Logger logger = Logger.getLogger("UserServiceImpl");
+        logger.info("doSomething" + user.getName());
     }
 }
